c3b59b88eebd6ccb8953de12d656833f362ec0d8
Zachary Tahsuda <zachar13y@gmail.com>
2019-09-17 22:32:51 -0700

    Feature/work in progress- added side menu animation.

--- JS/home.js
+++ JS/home.js
@@ -1,2 +1,22 @@
 /* eslint-disable no-console */
 'use strict';
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      link.style.animation = `navLinkfade 0.5s ease forwards $(index / 7 + 3)s`;
+    });
+  });
+
+
+};
+
+navSlide();
+
+console.log('hello');
